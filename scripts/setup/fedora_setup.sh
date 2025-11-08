1e21383b7d558443a7d0b2ea7a6d421bb0d3d305
RezkyKam50 <rezkykam34@gmail.com>
2025-11-09 00:01:19 +0700

    add deps

--- scripts/setup/fedora_setup.sh
+++ scripts/setup/fedora_setup.sh
@@ -1,4 +1,4 @@
 sudo dnf5 install \
-  make automake gcc gcc-c++ kernel-devel \
+  make automake kernel-devel \
   yasm cmake libtool numactl numactl-devel unzip wget \
-  nasm ninja gcc-14 g++-14
+  nasm ninja gcc-14 g++-14 aria2 uv 
--- tsvx.sh
+++ tsvx.sh
@@ -5,5 +5,3 @@ source ./scripts/setup/cv_lib.sh
 export PYTHONPATH="${PYTHONPATH}:${PWD}/src/bytetrack"
 __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia \ 
      python src/tsvx.py --source ./src/bytetrack/videos/car.mp4 --save-video --output ./footage/output.mp4
-
-#python src/spectralgraph.py --source 0  
