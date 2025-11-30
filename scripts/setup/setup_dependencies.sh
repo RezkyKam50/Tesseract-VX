cc7a8d965d09b144dad3337ddfb085c4b4770a44
RezkyKam50 <rezkykam34@gmail.com>
2025-11-30 13:00:15 +0000

    update

--- scripts/setup/setup_dependencies.sh
+++ scripts/setup/setup_dependencies.sh
@@ -3,5 +3,8 @@ source ./scripts/setup/gcc_switcher.sh
 
 uv pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu130
 uv pip install -v --no-build-isolation -U git+https://github.com/facebookresearch/xformers.git@main#egg=xformers
-python ./thirdparty/torch2trt/setup.py install
 uv pip install numba cupy numpy 
+cd ./thirdparty/torch2trt 
+uv pip install . --no-build-isolation
+cd .. && cd ..
+
